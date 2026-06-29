180a23525a2d60fe03a98b09f440c03365ce9a85
Lohith_creator <reddylohith99@gmail.com>
2026-06-29 16:20:52 +0530

    Salaried

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,12 @@ public class Main {
         System.out.println(tim);
         System.out.println("Age = " + tim.getAge());
         System.out.println("Pay = " + tim.collectpay());
+
+        SalariedEmployee john = new SalariedEmployee("john", "12/12/1990", "01/01/2019", 50000);
+        System.out.println(john);
+        System.out.println("john's Paycheck = $" + john.collectpay());
+
+        john.retired();
+        System.out.println("john's pension check = $" + john.collectpay());
     }
 }
---
+++ src/SalariedEmployee.java
@@ -0,0 +1,24 @@
+public class SalariedEmployee extends Employee{
+
+    double annualSalary;
+    boolean isRetired;
+
+    public SalariedEmployee(String name, String birthDate, String hireDate, double annualSalary) {
+        super(name, birthDate, hireDate);
+        this.annualSalary = annualSalary;
+    }
+
+    @Override
+    public double collectpay() {
+
+        double paycheck = annualSalary / 26;
+        double adjustedPay = (isRetired) ? 0.9 * paycheck : paycheck;
+
+        return (int) adjustedPay;
+    }
+
+    public void retired(){
+        terminate("12/12/2022");
+        isRetired = true;
+    }
+}
